1b7cbd72f8e6bc86d1e3ae0de899ad582a6d987b
Antonela Borgogno <antonelaborgogno@gmail.com>
2024-09-25 21:25:21 -0300

    modal

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+ const modal = document.querySelector(".modal");
+
+ console.log(modal);
+
+// modal.showModal(); aqui el dialog es un modal... se hace foco en el dialog, con un cambio de fondo y el dialog persigue por la pantalla
+
+// modal.show(); aqui solo es un dialog, no se foco en el cuadro
+
+
+modal.showModal();
